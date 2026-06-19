993aa42c1e91e30e6cb5e485eeaa54ddaf63ec7a
Dhanraj-SH <dhanrajsh445@gmail.com>
2026-06-19 20:15:23 +0530

    added organization and members route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const {authMiddleware} = require("./middleware");
 
 const app = express();
 
@@ -52,7 +53,7 @@ app.post("/signin", (req, res)=>{
     }
 
     const token = jwt.sign({
-        userID: userExists.id
+        userId: userExists.id
     },"token");
 
     res.json({
@@ -61,12 +62,55 @@ app.post("/signin", (req, res)=>{
 
 });
 
-app.post("/organization", (req, res)=>{
+app.post("/organization", authMiddleware, (req, res)=>{
+    const userId = req.userId;
+    ORGANIZATION.push({
+        id: ORGANIZATION_ID++,
+        title: req.body.title,
+        description: req.body.des,
+        admin: userId,
+        members: []
+    })
 
+    res.json({
+        message: "Org created",
+        id: ORGANIZATION_ID - 1 
+    });
 });
 
-app.post("/add_member_to_organization", (req, res)=>{
+app.post("/add_member_to_organization", authMiddleware, (req, res)=>{
+    const userId = req.userId;
+    const organizationId = req.body.organizationId;
+    const memberUsername = req.body.memberUsername;
 
+    const organization = ORGANIZATION.find(org => org.id === organizationId);
+
+    if(!organization || organization.admin != userId){
+        return res.status(403).json({
+            message: "Either the org does'nt exist or you are not an admin of this org"
+        });
+    }
+
+    const user = USERS.find(users => users.username === memberUsername);
+
+    if(!user){
+        return res.status(403).json({
+            message: "No user with this username exists in DB"
+        });
+    }
+
+    const userExists = organization.members.includes(user.id);
+
+    if(userExists){
+        return res.status(403).json({
+            message: "Member already exits in this organization"
+        })
+    }else{
+        organization.members.push(user.id);
+        res.json({
+            message: "New member added to org"
+        });
+    } 
 });
 
 app.post("/board", (req, res)=>{
@@ -89,12 +133,70 @@ app.get("/members", (req, res)=>{
 
 });
 
+app.get("/organization", authMiddleware, (req, res)=>{
+    const userId = req.userId;
+    const organizationId = parseInt(req.query.organizationId);
+
+    const organization = ORGANIZATION.find(org => org.id === organizationId);
+
+    if(!organization || organization.admin !== userId){
+        return res.status(403).json({
+            message: "Either the org does'nt exist or you are not an admin of this org"
+        })
+    }
+
+    res.json({
+        organization: {
+            ...organization,
+            members: organization.members.map(memberId => {
+                const user = USERS.find(user => user.id === memberId);
+                return {
+                    id: user.id,
+                    username: user.username
+                }
+            })
+        }
+    });
+});
+
 app.put("/issues", (req, res)=>{
 
 });
 
-app.delete("/signup", (req, res)=>{
+app.delete("/members", authMiddleware, (req, res)=>{
+    const userId = req.userId;
+    const organizationId = req.body.organizationId;
+    const memberUsername = req.body.memberUsername;
 
+    const organization = ORGANIZATION.find(org => org.id === organizationId);
+
+    if(!organization || organization.admin != userId){
+        return res.status(403).json({
+            message: "Either the org does'nt exist or you are not an admin of this org"
+        });
+    }
+
+    const user = USERS.find(users => users.username === memberUsername);
+
+    if(!user){
+        return res.status(403).json({
+            message: "No user with this username exists in DB"
+        });
+    }
+
+    const userExists = organization.members.includes(user.id);
+
+    if(!userExists){
+        return res.status(403).json({
+            message: "Member has been already removed from the organization"
+        })
+    }
+
+    organization.members = organization.members.filter(memberId => memberId !== user.id);
+
+    res.json({
+        message: "Member deleted from the org"
+    });
 });
 
 app.listen(3000);
---
+++ middleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next){
+    const token = req.headers.token;
+
+    if(!token){
+        return res.status(403).json({
+            message: "You have not logged in"
+        });
+    }
+
+    const decoded = jwt.verify(token, "token");
+    const userId = decoded.userId;
+
+    if(!userId){
+        return res.status(403).json({
+            message: "Malformed Token"
+        });
+    }
+
+    req.userId = userId;
+
+    next();
+
+}
+
+module.exports = {
+    authMiddleware
+}
